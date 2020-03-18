88829bc962e304ca6f07380935a9085a6ea05a1c
masayuki <masapyon@hotmail.co.jp>
2020-03-18 20:24:37 +0900

    add windows app base project using Qt5 toolkit

---
+++ PhotoStudioQt/MainWindow.cpp
@@ -0,0 +1,64 @@
+﻿//#include <PlatformQt.h>
+#include <Platform.h>
+
+#include <QtWidgets>
+
+#include "MainWindow.h"
+#include "ui_MainWindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+	: QMainWindow(parent)
+	, ui(new Ui::MainWindow)
+{
+	qApp->installEventFilter(this);
+
+	// initialize and show main window
+	ui->setupUi(this);
+
+	// create main menu
+//	this->createFileMenu();
+
+//	this->showMaximized();
+}
+
+MainWindow::~MainWindow()
+{
+
+	delete ui;
+}
+
+void MainWindow::closeEvent(QCloseEvent *event)
+{
+}
+
+bool MainWindow::eventFilter(QObject *widget, QEvent *event)
+{
+	if(event->type() == QEvent::KeyPress)
+	{
+		// キーボードショートカットが使用できるように
+		// aboutToShow に接続しているスロットを呼び出す
+//		this->fileToShow();
+	}
+
+	return false;
+}
+
+void MainWindow::dragEnterEvent(QDragEnterEvent *event)
+{
+	event->acceptProposedAction();
+}
+
+void MainWindow::dropEvent(QDropEvent *event)
+{
+//	QMessageBox msgBox(this);
+
+	//xxx
+}
+
+void MainWindow::moveEvent(QMoveEvent * event)
+{
+}
+
+void MainWindow::resizeEvent(QResizeEvent *event)
+{
+}
---
+++ PhotoStudioQt/main.cpp
@@ -0,0 +1,19 @@
+﻿#include <Platform.h>
+
+#include "MainWindow.h"
+
+#include <QApplication>
+
+INT main(INT argc, CHAR *argv[])
+{
+	QApplication app(argc, argv);
+
+	// resource
+//	Q_INIT_RESOURCE(PhotoStudioQt);
+
+	// main window
+	MainWindow window;
+	window.show();
+
+	return app.exec();
+}
