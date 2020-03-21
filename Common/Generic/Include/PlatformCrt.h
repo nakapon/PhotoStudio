5f4b8b300c16e8370fbe3d2c7097410653adaa73
masayuki <masapyon@hotmail.co.jp>
2020-03-21 23:06:56 +0900

    add PlatformCrt

---
+++ Common/Generic/Include/PlatformCrt.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+#if PLATFORM_WINDOWS
+	#include <WindowsPlatformCrt.h>
+#elif PLATFORM_LINUX
+	#include <LinuxPlatformCrt.h>
+#elif PLATFORM_MAC
+	#include <MacPlatformCrt.h>
+#else
+	#error Unknown Platform
+#endif
---
+++ Common/Linux/Include/LinuxPlatformCrt.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <new>
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+
+#include <math.h>
+#include <float.h>
+
+#include <limits.h>
+#include <wchar.h>
+
+#include <WindowsString.h>
+#include <WindowsTchar.h>
