6ce88af4968b95a750b2dce005719c60d6794ebd
masayuki <masapyon@hotmail.co.jp>
2020-03-26 00:09:05 +0900

    add AudioStudio project

---
+++ Project/AudioStudio/MainWindow.cpp
@@ -0,0 +1,341 @@
+#include <Platform.h>
+
+#include <AudioData.h>
+
+#include <WaveAudioReader.h>
+#include <WaveAudioWriter.h>
+
+#include <MenuLibrary.h>
+
+#include "Session.h"
+
+#include "resource.h"
+
+#define FILE_MENU_POS	0
+#define AUDIO_MENU_POS	1
+
+extern HINSTANCE g_hInstance;
+
+static CAudioData gs_AudioData;
+static CAudioData gs_ProcAudio;
+
+static void UpdateWaveform(HWND hWindow);
+static void LoadAudio(HWND hWindow, LPCTSTR pszFilePath, bool bShowErrorMsg);
+static void SaveAudio(HWND hWindow, LPCTSTR pszFilePath, const IAudioData* pAudioData);
+
+static void UpdateAppTitle(HWND hWindow);
+
+static void OnFileMenuPopup(HMENU hMenu);
+static void OnAudioMenuPopup(HMENU hMenu);
+
+// メインウィンドウ作成時の処理
+INT OnCreate(HWND hWindow, CREATESTRUCT* pCreateStruct)
+{
+	// ウィンドウにアイコンを設定
+	{
+		HICON hIcon = LoadIcon(g_hInstance, MAKEINTRESOURCE(IDI_SYMBOL));
+		SendMessage(hWindow, WM_SETICON, ICON_BIG, (WPARAM)hIcon);
+		SendMessage(hWindow, WM_SETICON, ICON_SMALL, (WPARAM)hIcon);
+	}
+
+	// ファイルドロップ許可
+	DragAcceptFiles(hWindow, TRUE);
+	
+	// 前回のオーディオデータを読み込む
+	{
+		TCHAR szFilePath[MAX_PATH] = { 0 };
+
+		if(Session::RestoreSession(szFilePath, PF_ARRAY_LENGTH(szFilePath)))
+		{
+			LoadAudio(hWindow, szFilePath, false);
+		}
+	}
+
+	UpdateAppTitle(hWindow);
+
+	return 0;
+}
+
+// メインウィンドウの「閉じる」ボタン押下時の処理
+INT OnClose(HWND hWindow)
+{
+	DestroyWindow(hWindow);
+
+	Session::StoreSession(gs_AudioData.GetAudioName());
+
+	return 0;
+}
+
+// メインウィンドウの破棄時の処理
+INT OnDestroy(HWND hWindow)
+{
+	// メッセージループを抜ける為に WM_QUIT メッセージを投げる
+	PostQuitMessage(0);
+
+	return 0;
+}
+
+// メインウィンドウの再描画時の処理
+INT OnPaint(HWND hWindow)
+{
+	HDC hDC;
+	PAINTSTRUCT PaintStruct = { 0 };
+
+	RECT rcClient;
+	SIZE ClientSize;
+
+	GetClientRect(hWindow, &rcClient);
+
+	ClientSize.cx = rcClient.right - rcClient.left;
+	ClientSize.cy = rcClient.bottom - rcClient.top;
+
+	hDC = BeginPaint(hWindow, &PaintStruct);
+
+	// 一旦画面を黒塗りする
+	{
+		HBRUSH hOldBrush = (HBRUSH)SelectObject(hDC, GetStockObject(BLACK_BRUSH));
+
+		Rectangle(hDC, 0, 0, ClientSize.cx, ClientSize.cy);
+
+		SelectObject(hDC, hOldBrush);
+	}
+
+	// xxx TODO draw waveform
+
+	EndPaint(hWindow, &PaintStruct);
+
+	return 0;
+}
+
+// メインウィンドウのコマンド（メニュー押下，ボタン押下など）処理
+INT OnCommand(HWND hWindow, WPARAM wParam, LPARAM lParam)
+{
+	switch(LOWORD(wParam))
+	{
+	case ID_FILE_NEW:
+		// xxx TODO
+		break;
+
+	case ID_FILE_OPEN:
+		{
+			OPENFILENAME OpenFileName = { 0 };
+
+			TCHAR szFilePath[MAX_PATH] = { 0 };
+
+			// 「ファイル選択ダイアログ」を表示
+			OpenFileName.lStructSize     = sizeof(OPENFILENAME);
+			OpenFileName.hwndOwner       = hWindow;
+			OpenFileName.lpstrFilter     = TEXT("WAVE (*.wav)\0*.wav\0\0");
+//			OpenFileName.lpstrFilter     = TEXT("All Files(*.*)\0*.*\0\0");
+			OpenFileName.nFilterIndex    = 1;
+			OpenFileName.lpstrFile       = szFilePath;
+			OpenFileName.nMaxFile        = MAX_PATH;
+			OpenFileName.Flags           = OFN_FILEMUSTEXIST | OFN_HIDEREADONLY;
+			OpenFileName.lpstrDefExt     = TEXT("");
+			OpenFileName.nMaxFileTitle   = MAX_PATH;
+			OpenFileName.lpstrFileTitle  = nullptr;
+			OpenFileName.lpstrTitle      = nullptr;
+
+			if(GetOpenFileName(&OpenFileName))
+			{
+				LoadAudio(hWindow, szFilePath, true);
+
+				UpdateAppTitle(hWindow);
+			}
+		}
+		break;
+
+	case ID_FILE_CLOSE:
+		gs_AudioData.Destroy();
+		gs_ProcAudio.Destroy();
+		UpdateWaveform(hWindow);
+		break;
+
+	case ID_FILE_SAVE:
+		{
+			OPENFILENAME SaveFileName = { 0 };
+
+			TCHAR szFilePath[MAX_PATH] = { 0 };
+
+			// 「ファイル選択ダイアログ」を表示
+			SaveFileName.lStructSize     = sizeof(OPENFILENAME);
+			SaveFileName.hwndOwner       = hWindow;
+			SaveFileName.lpstrFilter     = TEXT("WAVE (*.wav)\0*.wav\0\0");
+//			SaveFileName.lpstrFilter     = TEXT("All Files(*.*)\0*.*\0\0");
+			SaveFileName.nFilterIndex    = 1;
+			SaveFileName.lpstrFile       = szFilePath;
+			SaveFileName.nMaxFile        = MAX_PATH;
+			SaveFileName.Flags           = OFN_OVERWRITEPROMPT;
+			SaveFileName.lpstrDefExt     = TEXT("");
+			SaveFileName.nMaxFileTitle   = MAX_PATH;
+			SaveFileName.lpstrFileTitle  = nullptr;
+			SaveFileName.lpstrTitle      = nullptr;
+
+			if(GetSaveFileName(&SaveFileName))
+			{
+				IAudioData* pAudioData = gs_ProcAudio.IsCreated() ? &gs_ProcAudio : &gs_AudioData;
+
+				SaveAudio(hWindow, szFilePath, pAudioData);
+			}
+		}
+		break;
+
+	case ID_FILE_EXIT:
+		PostMessage(hWindow, WM_CLOSE, 0, 0);
+		break;
+
+	case ID_AUDIO_PLAY:
+		break;
+	}
+
+	return 0;
+}
+
+void OnFileMenuPopup(HMENU hMenu)
+{
+	MenuLibrary::Enable(hMenu, ID_FILE_CLOSE, false, gs_AudioData.IsCreated());
+	MenuLibrary::Enable(hMenu, ID_FILE_SAVE, false, gs_AudioData.IsCreated());
+}
+
+void OnAudioMenuPopup(HMENU hMenu)
+{
+	MenuLibrary::Enable(hMenu, ID_AUDIO_PLAY, false, gs_AudioData.IsCreated());
+}
+
+// メインウィンドウの通知処理
+INT OnNotify(HWND hWindow, WPARAM wParam, LPARAM lParam)
+{
+	return 0;
+}
+
+// メインウィンドウのスクロールバーの処理
+INT OnScrollEvent(HWND hWindow, UINT Message, WPARAM wParam, LPARAM lParam)
+{
+	return 0;
+}
+
+// メインウィンドウのマウスイベント処理
+INT OnMouseEvent(HWND hWindow, UINT Message, WPARAM wParam, LPARAM lParam)
+{
+	return 0;
+}
+
+// メインウィンドウのキー処理
+INT OnKeyEvent(HWND hWindow, UINT Message, WPARAM wParam, LPARAM lParam)
+{
+	return 0;
+}
+
+// メニューを展開するときの処理
+INT OnInitMenuPopup(HWND hWindow, WPARAM wParam, LPARAM lParam)
+{
+	HMENU hTargetMenu = (HMENU)wParam;
+
+	UInt32 Position = LOWORD(lParam);
+	BOOL bWindowMenu = (BOOL)HIWORD(lParam);
+
+	HMENU hParentMenu = ::GetMenu(hWindow);
+
+	HMENU hMenu = ::GetSubMenu(hParentMenu, FILE_MENU_POS);
+	if(hMenu == hTargetMenu)
+	{
+		OnFileMenuPopup(hMenu);
+	}
+
+	hMenu = ::GetSubMenu(hParentMenu, AUDIO_MENU_POS);
+	if(hMenu == hTargetMenu)
+	{
+		OnAudioMenuPopup(hMenu);
+	}
+
+	return 0;
+}
+
+// ファイルをウィンドウにドロップしたときの処理
+INT OnDropFiles(HWND hWindow, WPARAM wParam, LPARAM lParam)
+{
+	HDROP hDrop = (HDROP)wParam;
+
+	TCHAR szFilePath[MAX_PATH] = { 0 };
+
+	UInt32 ItemCount = DragQueryFile(hDrop, (UInt32)-1, nullptr, 0);
+
+	DragQueryFile(hDrop, 0, szFilePath, PF_ARRAY_LENGTH(szFilePath));
+
+	LoadAudio(hWindow, szFilePath, true);
+
+	UpdateAppTitle(hWindow);
+
+	return 0;
+}
+
+static void UpdateWaveform(HWND hWindow)
+{
+	RedrawWindow(hWindow, nullptr, nullptr, RDW_INVALIDATE | RDW_ERASENOW | RDW_UPDATENOW);
+}
+
+static void LoadAudio(HWND hWindow, LPCTSTR pszFilePath, bool bShowErrorMsg)
+{
+	// 処理結果を破棄
+	gs_ProcAudio.Destroy();
+
+	if(!WaveAudioReader::ReadAudio(pszFilePath, &gs_AudioData))
+	{
+		// 読み込み失敗
+		if(bShowErrorMsg)
+		{
+			MessageBox(hWindow, TEXT("Failed to load the specified file."), TEXT("Error"), MB_ICONERROR | MB_OK | MB_TOPMOST);
+		}
+	}
+	else
+	{
+		// 読み込み成功 - メインウィンドウを再描画
+		UpdateWaveform(hWindow);
+	}
+}
+
+static void SaveAudio(HWND hWindow, LPCTSTR pszFilePath, const IAudioData* pAudioData)
+{
+
+}
+
+static void UpdateAppTitle(HWND hWindow)
+{
+	TCHAR szTitle[256] = { 0 };
+
+	PFString::Copy(szTitle, TEXT("Audio Studio"));
+
+	if(gs_AudioData.IsCreated())
+	{
+		PFString::Append(szTitle, TEXT(" - "));
+
+		// オーディオ名 (ファイルパスの場合はファイル名部分だけを表示）
+		{
+			LPCTSTR pszAudioName;
+
+			pszAudioName = PFString::Strrchr(gs_AudioData.GetAudioName(), PF_DIR_SEP_CHAR);
+			if(pszAudioName != nullptr)
+			{
+				pszAudioName++;
+			}
+			else
+			{
+				pszAudioName =gs_AudioData.GetAudioName();
+			}
+
+			PFString::Append(szTitle, pszAudioName);
+		}
+
+		// オーディオ情報
+		{
+			TCHAR szInfo[128] = { 0 };
+
+			IAudioData::AUDIOINFO AudioInfo = gs_AudioData.GetAudioInfo();
+
+			_stprintf_s(szInfo, TEXT(" [%dch %dbit %dHz %d samples]"), AudioInfo.ChannelCount, AudioInfo.BitsPerChannel, AudioInfo.SamplesPerSec, AudioInfo.SampleCount);
+			PFString::Append(szTitle, szInfo);
+		}
+	}
+
+	SetWindowText(hWindow, szTitle);
+}
---
+++ Project/AudioStudio/WinMain.cpp
@@ -0,0 +1,181 @@
+#include <Platform.h>
+
+// メモリリーク検知有効
+#define __DETECT_MEMORY_LEAKS__
+
+#ifdef __DETECT_MEMORY_LEAKS__
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+#endif // __DETECT_MEMORY_LEAKS__
+
+#include "resource.h"
+
+// ウィンドウタイトル
+#define APP_WINDOW_NAME	TEXT("Audio Studio")
+
+// ウィンドウクラス
+#define APP_CLASS_NAME	TEXT("AudioStudioClass")
+
+// ウィンドウサイズ
+#define WINDOW_WIDTH	1200
+#define WINDOW_HEIGHT	800
+
+HINSTANCE g_hInstance;
+
+INT OnCreate(HWND hWindow, CREATESTRUCT* pCreateStruct);
+INT OnClose(HWND hWindow);
+INT OnDestroy(HWND hWindow);
+INT OnPaint(HWND hWindow);
+INT OnCommand(HWND hWindow, WPARAM wParam, LPARAM lParam);
+INT OnNotify(HWND hWindow, WPARAM wParam, LPARAM lParam);
+INT OnScrollEvent(HWND hWindow, UINT Message, WPARAM wParam, LPARAM lParam);
+INT OnMouseEvent(HWND hWindow, UINT Message, WPARAM wParam, LPARAM lParam);
+INT OnKeyEvent(HWND hWindow, UINT Message, WPARAM wParam, LPARAM lParam);
+INT OnInitMenuPopup(HWND hWindow, WPARAM wParam, LPARAM lParam);
+INT OnDropFiles(HWND hWindow, WPARAM wParam, LPARAM lParam);
+
+static LRESULT CALLBACK WindowProcedure(HWND hWindow, UINT Message, WPARAM wParam, LPARAM lParam)
+{
+	switch(Message)
+	{
+	case WM_CREATE:
+		return OnCreate(hWindow, (CREATESTRUCT*)lParam);
+
+	case WM_CLOSE:
+		return OnClose(hWindow);
+
+	case WM_DESTROY:
+		return OnDestroy(hWindow);
+
+	case WM_PAINT:
+		return OnPaint(hWindow);
+
+	case WM_COMMAND:
+		return OnCommand(hWindow, wParam, lParam);
+
+	case WM_NOTIFY:
+		return OnNotify(hWindow, wParam, lParam);
+
+	case WM_HSCROLL:	case WM_VSCROLL:
+		return OnScrollEvent(hWindow, Message, wParam, lParam);
+
+	case WM_LBUTTONDOWN: case WM_LBUTTONUP:
+	case WM_RBUTTONDOWN: case WM_RBUTTONUP:
+	case WM_MOUSEMOVE:
+		return OnMouseEvent(hWindow, Message, wParam, lParam);
+
+	case WM_KEYDOWN:	case WM_KEYUP:
+	case WM_CHAR:
+		return OnKeyEvent(hWindow, Message, wParam, lParam);
+
+	case WM_INITMENUPOPUP:
+		return OnInitMenuPopup(hWindow, wParam, lParam);
+
+	case WM_DROPFILES:
+		return OnDropFiles(hWindow, wParam, lParam);
+
+	default:
+		return DefWindowProc(hWindow, Message, wParam, lParam);
+	}
+
+	return 0;
+}
+
+static BOOL InitializeApplication(HINSTANCE hInstance)
+{
+	WNDCLASSEX WindowClassEx = { 0 };
+
+	WindowClassEx.cbSize = sizeof(WindowClassEx);
+	WindowClassEx.style = CS_HREDRAW | CS_VREDRAW;
+	WindowClassEx.lpfnWndProc = WindowProcedure;
+	WindowClassEx.cbClsExtra = 0;
+	WindowClassEx.cbWndExtra = 0;
+	WindowClassEx.hInstance = hInstance;
+	WindowClassEx.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	WindowClassEx.hCursor = LoadCursor(NULL, IDC_ARROW);
+	WindowClassEx.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	WindowClassEx.lpszMenuName = NULL;
+	WindowClassEx.lpszClassName = APP_CLASS_NAME;
+	WindowClassEx.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+	return (RegisterClassEx(&WindowClassEx) == 0) ? FALSE : TRUE;
+}
+
+static BOOL CreateMainWindow(HINSTANCE hInstance, INT nShowCommand, HWND &hWindow)
+{
+	HMENU hMainMenu = LoadMenu(hInstance, MAKEINTRESOURCE(IDR_MAIN_MENU));
+
+	hWindow = CreateWindowEx(0, APP_CLASS_NAME, APP_WINDOW_NAME,
+							 WS_OVERLAPPEDWINDOW, 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT,
+							 NULL, hMainMenu, hInstance, NULL);
+	if(hWindow == NULL)
+		return FALSE;
+
+	ShowWindow(hWindow, nShowCommand);
+
+	UpdateWindow(hWindow);
+
+	return TRUE;
+}
+
+// WinMain - Windowsアプリの場合はエントリポイントは main ではなく WinMain
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pszCommandLine, INT nShowCommand)
+{
+	INT nReturn;
+
+	HWND hWindow;
+
+	g_hInstance = hInstance;
+
+	CoInitialize(NULL);
+
+#ifdef __DETECT_MEMORY_LEAKS__
+	//_CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_WNDW | _CRTDBG_MODE_DEBUG);
+	//_CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_WNDW | _CRTDBG_MODE_DEBUG);
+	//_CrtSetReportMode(_CRT_ASSERT, _CRTDBG_MODE_WNDW | _CRTDBG_MODE_DEBUG);
+
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif // __DETECT_MEMORY_LEAKS__
+
+	// アプリケーション初期化 - ウィンドウクラスの登録
+	if(!InitializeApplication(hInstance))
+		return -1;
+
+	// メインウィンドウの作成
+	if(!CreateMainWindow(hInstance, nShowCommand, hWindow))
+		return -1;
+
+	// メッセージループ．メインウィンドウを閉じる(WM_QUIT メッセージを受信する)まで無限ループ．
+	{
+		INT nResult = 0;
+
+		MSG Message;
+		HACCEL hAccel;
+
+		hAccel = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDR_ACCELERATOR));
+
+		// GetMessage は WM_QUIT を受信すると FALSE を返す
+		while((nResult = GetMessage(&Message, NULL, 0, 0)) != 0)
+		{
+			if(nResult == -1)
+			{
+				MessageBox(hWindow, TEXT("Fatal error occurred at message loop!!"), TEXT("Error"), MB_ICONERROR | MB_TOPMOST | MB_OK);
+			}
+			else
+			{
+				if(!TranslateAccelerator(hWindow, hAccel, &Message))
+				{
+					TranslateMessage(&Message);
+					DispatchMessage(&Message);
+				}
+			}
+		}
+
+		nReturn = (INT)Message.wParam;
+	}
+
+	CoUninitialize();
+
+	return nReturn;
+}
